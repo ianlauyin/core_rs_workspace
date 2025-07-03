ba019ef9779c22b87cc4ed9874d4da2980f67f70
neo <1100909+neowu@users.noreply.github.com>
2025-07-03 17:03:30 +0800

    Return Result from main in scheduler example

--- app/log_exporter/examples/scheduler.rs
+++ app/log_exporter/examples/scheduler.rs
@@ -14,7 +14,7 @@ use tracing::warn;
 struct State {}
 
 #[tokio::main]
-pub async fn main() {
+pub async fn main() -> Result<()> {
     log::init_with_action(ConsoleAppender);
 
     let shutdown = Shutdown::new();
@@ -23,7 +23,7 @@ pub async fn main() {
     let mut scheduler = Scheduler::new(FixedOffset::east_opt(8 * 60 * 60).unwrap());
     scheduler.schedule_fixed_rate("test", job, Duration::from_secs(1));
     scheduler.schedule_daily("test-daily", daily_job, NaiveTime::from_hms_opt(17, 28, 50).unwrap());
-    scheduler.start(Arc::new(State {}), signal).await;
+    scheduler.start(Arc::new(State {}), signal).await
 }
 
 async fn job(_state: Arc<State>, context: JobContext) -> Result<()> {
