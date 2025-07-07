3905b14da2c342cec166224f23cca81ad83e7925
neo <1100909+neowu@users.noreply.github.com>
2025-07-07 15:22:13 +0800

    Refactor log upload to use helper for conversion and upload

--- app/log_exporter/src/service.rs
+++ app/log_exporter/src/service.rs
@@ -44,57 +44,42 @@ pub async fn upload_archive(date: NaiveDate, state: &Arc<AppState>) -> Result<()
 
     let action_log_path = local_file_path("action", date, state)?;
     if action_log_path.exists() {
-        let local_path = action_log_path.to_string_lossy();
-        let parquet_path_buf = action_log_path.with_extension("parquet");
-        let parquet_path = parquet_path_buf.to_string_lossy();
-
-        let command = &format!(
-            r#"SET memory_limit = '256MB';SET temp_directory = '/tmp/duckdb';COPY (SELECT * FROM read_ndjson( ['{local_path}'],
-            columns = {{'date': 'TIMESTAMPTZ', id: 'STRING', app: 'STRING', host: 'STRING', result: 'STRING', action: 'STRING', ref_ids: 'STRING[]', correlation_ids: 'STRING[]', clients: 'STRING[]', error_code: 'STRING', error_message: 'STRING', elapsed: 'LONG', context: 'MAP(STRING, STRING[])', stats: 'MAP(STRING, DOUBLE)', perf_stats: 'MAP(STRING, MAP(STRING, DOUBLE))'}}
-        )) TO '{parquet_path}' (FORMAT 'parquet');"#
-        );
-        shell::run(&format!("duckdb -c \"{command}\"")).await?;
-
         let remote_path = remote_path("action", date, state);
-
-        // requires '-q', otherwise standard output may block if buffer is full, Shell reads std after process ends
-        // and '-m' may stress network bandwidth, currently not really necessary
-        let bucket = &state.bucket;
-        let command = format!("gcloud storage cp --quiet cp {parquet_path} {bucket}/{remote_path}",);
-        shell::run(&command).await?;
-
-        fs::remove_file(parquet_path_buf)?;
+        let columns = "{'date': 'TIMESTAMPTZ', id: 'STRING', app: 'STRING', host: 'STRING', result: 'STRING', action: 'STRING', ref_ids: 'STRING[]', correlation_ids: 'STRING[]', clients: 'STRING[]', error_code: 'STRING', error_message: 'STRING', elapsed: 'LONG', context: 'MAP(STRING, STRING[])', stats: 'MAP(STRING, DOUBLE)', perf_stats: 'MAP(STRING, MAP(STRING, DOUBLE))'}";
+        convert_parquet_and_upload(action_log_path, &remote_path, columns).await?;
     }
 
     let event_path = local_file_path("event", date, state)?;
     if event_path.exists() {
-        let local_path = event_path.to_string_lossy();
-        let parquet_path_buf = event_path.with_extension("parquet");
-        let parquet_path = parquet_path_buf.to_string_lossy();
-
-        let command = &format!(
-            r#"SET memory_limit = '256MB';SET temp_directory = '/tmp/duckdb';COPY (SELECT * FROM read_ndjson( ['{local_path}'],
-            columns = {{'date': 'TIMESTAMPTZ', id: 'STRING', app: 'STRING', received_time: 'TIMESTAMPTZ', result: 'STRING', action: 'STRING', error_code: 'STRING', error_message: 'STRING', elapsed: 'LONG', context: 'MAP(STRING, STRING)', stats: 'MAP(STRING, DOUBLE)', info: 'MAP(STRING, STRING)'}}
-        )) TO '{parquet_path}' (FORMAT 'parquet');"#
-        );
-        shell::run(&format!("duckdb -c \"{command}\"")).await?;
-
         let remote_path = remote_path("event", date, state);
-
-        // requires '-q', otherwise standard output may block if buffer is full, Shell reads std after process ends
-        // and '-m' may stress network bandwidth, currently not really necessary
-        let bucket = &state.bucket;
-        let command = format!("gcloud storage cp --quiet cp {parquet_path} {bucket}/{remote_path}",);
-        shell::run(&command).await?;
-
-        fs::remove_file(parquet_path_buf)?;
+        let columns = "{'date': 'TIMESTAMPTZ', id: 'STRING', app: 'STRING', received_time: 'TIMESTAMPTZ', result: 'STRING', action: 'STRING', error_code: 'STRING', error_message: 'STRING', elapsed: 'LONG', context: 'MAP(STRING, STRING)', stats: 'MAP(STRING, DOUBLE)', info: 'MAP(STRING, STRING)'}";
+        convert_parquet_and_upload(event_path, &remote_path, columns).await?;
     }
 
     Ok(())
 }
 
+async fn convert_parquet_and_upload(
+    local_path_buf: PathBuf,
+    remote_path: &str,
+    columns: &str,
+) -> Result<(), anyhow::Error> {
+    let local_path = local_path_buf.to_string_lossy();
+    let parquet_path_buf = local_path_buf.with_extension("parquet");
+    let parquet_path = parquet_path_buf.to_string_lossy();
+    let command = format!(
+        r#"SET memory_limit='256MB';SET temp_directory='/tmp/duckdb';COPY (SELECT * FROM read_ndjson(['{local_path}'], columns = {columns})) TO '{parquet_path}' (FORMAT 'parquet');"#
+    );
+    shell::run(&format!("duckdb -c \"{command}\"")).await?;
+    let command = format!("gcloud storage cp --quiet {parquet_path} {remote_path}");
+    shell::run(&command).await?;
+    fs::remove_file(parquet_path_buf)?;
+    Ok(())
+}
+
 fn remote_path(name: &str, date: NaiveDate, state: &Arc<AppState>) -> String {
+    let bucket = &state.bucket;
     let year = date.year();
     let hash = &state.hash;
-    format!("/{name}/{year}/{name}-{date}-{hash}.parquet")
+    format!("{bucket}/{name}/{year}/{name}-{date}-{hash}.parquet")
 }
