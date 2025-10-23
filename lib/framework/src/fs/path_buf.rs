3d5608757e3087a6ce735e58de59118aa0f3e056
neo <1100909+neowu@users.noreply.github.com>
2025-10-23 17:05:40 +0800

    Update path_buf.rs

--- lib/framework/src/fs/path_buf.rs
+++ lib/framework/src/fs/path_buf.rs
@@ -10,15 +10,15 @@ pub trait PathBufExt {
 
 impl PathBufExt for PathBuf {
     fn into_absolute_path(self) -> Result<PathBuf, Exception> {
-        if self.is_absolute() {
-            return Ok(self);
-        }
-        let current_dir =
-            env::current_dir().map_err(|err| exception!(message = "failed to get current directory", source = err))?;
+        let absolute_path = if self.is_absolute() {
+            self
+        } else {
+            let current_dir = env::current_dir()
+                .map_err(|err| exception!(message = "failed to get current directory", source = err))?;
+            current_dir.join(self)
+        };
 
-        let absolute_path = current_dir.join(self);
         let cannonical_path = fs::canonicalize(absolute_path)?;
-
         Ok(cannonical_path)
     }
 }
